e1c53cb92922b6e59ba944d04cb819825c33c63e
veerapatr <S6752410008@sau.ac.th>
2025-02-04 17:00:33 +0700

    start commit

---
+++ src/com/sau/wow/DTISAU.java
@@ -0,0 +1,19 @@
+package com.sau.wow;
+
+public class DTISAU {
+    //data member ตัวข้อมูล
+    String name;
+    int Birthyear;
+    int age;
+    double high = 100;
+
+
+    //method member การทำงาน
+    public void showHi(){
+        System.out.println("Hi...");
+    }
+    public void calAndshowage(){
+        age = 2568 - Birthyear;
+        System.out.println("Your age" + age);
+    }
+}
---
+++ src/com/sau/wow/Test01.java
@@ -0,0 +1,8 @@
+package com.sau.wow;
+
+public class Test01 {
+    public static void main(String[] args) {
+        DTISAU mod;
+        mod = DTISAU();
+    }
+}
